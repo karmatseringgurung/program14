684939c70311b3b41bf39e992088491fc0da7a91
Priyash Kafle <kaflepr@mnstate.edu>
2016-04-27 11:41:10 -0500

    commited the intersection method,difference of sets method and boolean method

--- modCustomSet.py
+++ modCustomSet.py
@@ -3,3 +3,42 @@ Description: This is the modCustomSet
 __author__="Karma"
 __date__="4/22/16"
 """
+
+
+
+#Priyash Kafle
+def __and__(self,other):
+    '''
+    Description: Returns an intersection of two sets 
+    PreCOndition: other(list)
+    PostCondition: None
+    '''
+    newSet=[]
+    for i in self._setList:
+        if i in other._setList:
+            newSet.append(i)
+    otherSet=CustomSet(newSet)
+
+    return newSet
+
+def __sub__(self, other):
+    '''
+    Description: Returns the unique element of set 1
+    PreCondition: other(list)
+    PostCondition: None
+    '''
+    myList=[]
+    for item in self._setList:
+        if item not in other._setList:
+            myList=myList.append(item)
+
+def __conatins__(self,other):
+    '''
+    Description: Checks if an element is in the set
+    PreCOndition: other(integer)
+    POstCondition: None
+    '''
+    if other in self._setList:
+        return "yes"
+    else:
+        return "not there"
